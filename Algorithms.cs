60033c87db84a13711b81c8294515ab8d4acffbc
Shea Raymond <shea.raymond01@gmail.com>
2023-09-06 12:00:45 -0500

    Creating function IsEvenNumber and figuring its time complexity.

---
+++ Algorithms.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Algorithms_and_BigONotation
+{
+    public class Algorithms
+    {
+        public Algorithms()
+        {
+            
+        }
+        public bool IsEvenNumber(int numberToVerify) 
+        {
+            bool isEven = false;
+            if (numberToVerify % 2 == 0) 
+            {
+                isEven = true;
+            }
+            return isEven;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Algorithms myAlgos = new Algorithms();
+            
+            int numberToTest = 118;
+            for (int i = 0; i < 2; i++)
+            {
+                bool isEven = myAlgos.IsEvenNumber(numberToTest);
+                if (isEven)
+                {
+                    Console.WriteLine($"The number {numberToTest} is an even number");
+                }
+                else
+                {
+                    Console.WriteLine($"The number {numberToTest} is an not an even but an odd number");
+                }
+                numberToTest++;
+            }
         }
     }
 }
