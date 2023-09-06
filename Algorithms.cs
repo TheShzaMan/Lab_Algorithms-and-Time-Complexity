7f39d3d4bd5a7d1d98b29978f26a57de602fde55
Shea Raymond <shea.raymond01@gmail.com>
2023-09-06 17:52:14 -0500

    Adding functions for IsLessThan100 and HasMultipleNames along with their Time Complexity.

--- Algorithms.cs
+++ Algorithms.cs
@@ -12,6 +12,8 @@ namespace Lab_Algorithms_and_BigONotation
         {
             
         }
+
+        //Time Complexity for IsEvenNumber: O(1) Constant
         public bool IsEvenNumber(int numberToVerify) 
         {
             bool isEven = false;
@@ -21,5 +23,61 @@ namespace Lab_Algorithms_and_BigONotation
             }
             return isEven;
         }
+
+        //Time Complexity for IsLessThan100:   O(n) Linear
+        public bool IsLessThan100(List<int> listToCheck)
+        {
+            bool isLessThan100 = false;
+            for (int i = 0; i < listToCheck.Count; i++)
+            {
+                if (listToCheck[i] >= 100)
+                {
+                    isLessThan100 = false;
+                    break;
+                }
+                else
+                {
+                    isLessThan100 = true;
+                }
+            }
+            return isLessThan100;
+        }
+
+        //Time Complexity for HasMultipleNames:  O(n log n) Log Linear
+        public bool HasMultipleNames(List<string> listToCheck) 
+        {
+            bool hasMultipleNames = false;
+            for (int i = 0; i < listToCheck.Count; i++)
+            {
+                for (int j = (i + 1); j < listToCheck.Count; j++) 
+                {
+                    if (listToCheck[i].Equals(listToCheck[j]))
+                    {
+                        hasMultipleNames = true;
+                        break;
+                    }
+                }
+            }
+            return hasMultipleNames;
+        }
+
+        public List<int> SortToAscending(List<int> listToSort)
+        {
+            List<int> ascendingList = new List<int>();
+
+            for (int i = 0, j = 1; i < listToSort.Count; i++, j++)
+            {
+                if (listToSort[i] < listToSort[j])
+                {
+                    for (int k = 0; k < listToSort.Count - i; k++)
+                    {
+                        if (listToSort[i] < listToSort[])
+                    }
+                }
+            }
+            return ascendingList;
+        }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         {
             Algorithms myAlgos = new Algorithms();
             
+            //Task 1
             int numberToTest = 118;
             for (int i = 0; i < 2; i++)
             {
@@ -19,6 +20,52 @@
                     Console.WriteLine($"The number {numberToTest} is an not an even but an odd number");
                 }
                 numberToTest++;
+            }
+
+            //Task 2
+            List<int> numbersList = new List<int>() { 9, 4, 90, -8, 66, 99, 1, 43 };
+            List<int> numbersList2 = new List<int>() { 9, 4, 90, -8, 425, 99, 43};
+            
+            bool isLess = myAlgos.IsLessThan100(numbersList);
+            if ( isLess )
+            {
+                Console.WriteLine($"Every number in this list is less than 100");
+            }
+            else
+            {
+                Console.WriteLine($"One or more numbers in this list are more than 100");
+            }
+            isLess = myAlgos.IsLessThan100(numbersList2);
+            if (isLess)
+            {
+                Console.WriteLine($"Every number in this list is less than 100");
+            }
+            else
+            {
+                Console.WriteLine($"One or more numbers in this list are more than 100");
+            }
+
+            //Task 3
+            List<string> namesList = new List<string>() { "Martha", "Danielle", "Steven", "Mark" };
+            List<string> namesList2 = new List<string>() { "Martha", "Danielle", "Steven", "Martha", "Mark" };
+
+            bool hasMultiple = myAlgos.HasMultipleNames(namesList);
+            if ( hasMultiple )
+            {
+                Console.WriteLine("This list has multiple names");
+            }
+            else
+            {
+                Console.WriteLine("This list does not have multiple names");
+            }
+            hasMultiple = myAlgos.HasMultipleNames(namesList2);
+            if (hasMultiple)
+            {
+                Console.WriteLine("This list has multiple names");
+            }
+            else
+            {
+                Console.WriteLine("This list does not have multiple names");
             }
         }
     }
