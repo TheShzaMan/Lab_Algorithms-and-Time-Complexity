14cb5db08309f22fc70b62193b96de1f0ce63b23
Shea Raymond <shea.raymond01@gmail.com>
2023-09-06 11:20:52 -0500

    Initalizing repo in GitHub

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_Algorithms_and_BigONotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
